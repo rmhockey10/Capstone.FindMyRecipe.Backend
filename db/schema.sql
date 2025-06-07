32d99343c0141e8a4d66bd34db27e3a9edfab3d8
Devin Frazier <Dev.fraz18@gmail.com>
2025-06-07 11:39:26 -0400

    Created db schema and changed pasckage.json to reflect the DB name 'find_my_recipe'

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,34 @@
+DROP TABLE IF EXISTS recipes_ingredients;
+DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS ingredients;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
   username text NOT NULL UNIQUE,
   password text NOT NULL
+);
+
+CREATE TABLE recipes (
+id serial PRIMARY KEY,
+name text NOT NULL,
+instructions text NOT NULL,
+prep_time_minutes integer NOT NULL,
+cook_time_minutes integer NOT NULL,
+cuisine text NOT NULL,
+servings integer NOT NULL,
+difficulty text NOT NULL,
+calories_per_serving integer NOT NULL,
+image text
+);
+
+CREATE TABLE ingredients (
+  id serial PRIMARY KEY,
+  name text NOT NULL UNIQUE
+);
+
+CREATE TABLE recipes_ingredients (
+  recipes_id integer NOT NULL REFERENCES recipes(id) ON DELETE CASCADE,
+  ingredients_id integer NOT NULL REFERENCES ingredients(id) ON DELETE CASCADE,
+  PRIMARY KEY(recipes_id, ingredients_id)
 );
