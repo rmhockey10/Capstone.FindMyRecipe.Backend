7153c7da480b0168747e455b9d0324be857d6a56
Devin Frazier <Dev.fraz18@gmail.com>
2025-06-08 11:54:00 -0400

    Added getIngredients and getIngredientById queries

--- db/queries/ingredients.js
+++ db/queries/ingredients.js
@@ -29,3 +29,33 @@ export async function createIngredient(name) {
   } = await db.query(selectSql, [name]);
   return ingredient;
 }
+
+/**
+ * Retrieves all ingredients from the database.
+ * @returns {Promise<Array>} An array of all ingredient.
+ */
+export async function getIngredients() {
+  const sql = `
+  SELECT *
+  FROM ingredients
+  `;
+  const { rows: ingredients } = await db.query(sql);
+  return ingredients;
+}
+
+/**
+ * Retrieves a single ingredient by its ID.
+ * @param {number} id - The ID of the ingredient to retrieve.
+ * @returns {Promise<Object|undefined>} The ingredient.
+ */
+export async function getIngredientById(id) {
+  const sql = `
+  SELECT *
+  FROM ingredients
+  WHERE id = $1
+  `;
+  const {
+    rows: [ingredient],
+  } = await db.query(sql, [id]);
+  return ingredient;
+}
