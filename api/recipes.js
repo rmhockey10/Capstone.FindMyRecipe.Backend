ad06b4aaf99ffffa93275a9adf276e1450120928
eric <dorseyjr000@gmail.com>
2025-06-11 16:58:59 -0500

    fix api

--- api/recipes.js
+++ api/recipes.js
@@ -24,40 +24,34 @@ router
     res.send(recipes);
   })
 
-  .post(requireBody(["ingredients", "ingredients count"]), async (req, res) => {
-    const { ingredients, ingredientcount } = req.body;
-    const recipe = await getRecipeByIngredients(
-      ingredients,
-      ingredientcount,
-      req.user.id
-    );
+  .post(requireBody(["ingredients"]), async (req, res) => {
+    const { ingredients } = req.body;
+    const recipe = await getRecipeByIngredients(ingredients);
     res.status(201).send(recipe);
   });
 
 // Preload a single recipe by :id
 // Attaches the recipe to req.recipe if found, or sends 404
-router
-  .param("id", async (req, res, next, id) => {
-    const recipe = await getRecipeByIdWithIngredie(id);
-    if (!recipe) return res.status(404).send("recipe not found.");
-    req.recipe = recipe;
-    next();
-  })
-  .post(requireBody(["ingredients", "ingredients count"]), async (req, res) => {
-    const { ingredients, ingredientcount } = req.body;
-    const recipe = await getRecipeByIngredients(
-      ingredients,
-      ingredientcount,
-      req.user.id
-    );
-    res.status(201).send(recipe);
-  });
+router.param("id", async (req, res, next, id) => {
+  const recipe = await getRecipeByIdWithIngredie(id);
+  if (!recipe) return res.status(404).send("recipe not found.");
+  req.recipe = recipe;
+  next();
+});
 
 // GET /recipes/:id
 // Returns a single recipe (preloaded by router.param)
-router.route("/:id").get((req, res) => {
-  res.send(req.recipe);
-});
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(req.recipe);
+  })
+
+  .post(requireBody(["ingredients"]), async (req, res) => {
+    const { ingredients } = req.body;
+    const recipe = await getRecipeByIdWithIngredie(ingredients);
+    res.status(201).send(recipe);
+  });
 
 // GET /recipes
 // Returns all recipes in the database
